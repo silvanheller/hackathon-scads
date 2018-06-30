b2d0b329fb06096680979b752e3add82cc4d6849
Silvan Heller <silvan.heller@unibas.ch>
2018-06-30 14:43:07 +0200

    First Draft of stream processing by religion & country

--- hackathon-flink-scala/src/main/scala/eu/streamline/hackathon/flink/scala/job/FlinkScalaJob.scala
+++ hackathon-flink-scala/src/main/scala/eu/streamline/hackathon/flink/scala/job/FlinkScalaJob.scala
@@ -4,17 +4,14 @@ import java.util.Date
 
 import eu.streamline.hackathon.common.data.GDELTEvent
 import eu.streamline.hackathon.flink.operations.GDELTInputFormat
-import org.apache.flink.api.common.functions.FoldFunction
+import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.core.fs.Path
 import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
 import org.apache.flink.streaming.api.scala._
-import org.apache.flink.streaming.api.scala.function.WindowFunction
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows
 import org.apache.flink.streaming.api.windowing.time.Time
-import org.apache.flink.streaming.api.windowing.windows.TimeWindow
-import org.apache.flink.util.Collector
 
 object FlinkScalaJob {
 
@@ -22,47 +19,55 @@ object FlinkScalaJob {
 
     val parameters = ParameterTool.fromArgs(args)
     val pathToGDELT = parameters.get("path")
-    val country = parameters.get("country", "USA")
+    System.out.println("Path: " + pathToGDELT)
 
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
 
-    implicit val typeInfo = createTypeInformation[GDELTEvent]
-    implicit val dateInfo = createTypeInformation[Date]
+    implicit val typeInfo: TypeInformation[GDELTEvent] = createTypeInformation[GDELTEvent]
+    implicit val dateInfo: TypeInformation[Date] = createTypeInformation[Date]
 
     val source = env
       .readFile[GDELTEvent](new GDELTInputFormat(new Path(pathToGDELT)), pathToGDELT)
       .setParallelism(1)
 
-    source.filter((event: GDELTEvent) => {
-        event.actor1Code_countryCode != null &
-      event.actor1Code_countryCode == country
-    }).assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor[GDELTEvent](Time.seconds(0)) {
+    //To Discuss: Religion2Code?
+
+    val filteredStream: DataStream[GDELTEvent] = source
+      .filter((event: GDELTEvent) => {
+        event.goldstein != null &&
+          event.avgTone != null &&
+          event.quadClass != null
+      })
+      //Prevent Nullpointer exceptions*/
+      .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor[GDELTEvent](Time.seconds(0)) {
       override def extractTimestamp(element: GDELTEvent): Long = {
         element.dateAdded.getTime
       }
-    }).keyBy((event: GDELTEvent) => {
-      event.actor1Code_countryCode
-    }).window(TumblingEventTimeWindows.of(Time.days(1)))
-    .fold(
-        0.0,
-        new FoldFunction[GDELTEvent, Double] {
-          override def fold(accumulator: Double, value: GDELTEvent) = {
-            accumulator + value.avgTone
-          }
-        },
-        new WindowFunction[Double, (String, Double, Date, Date), String, TimeWindow] {
-          override def apply(key: String,
-                             window: TimeWindow,
-                             input: Iterable[Double],
-                             out: Collector[(String, Double, Date, Date)]): Unit = {
-            out.collect((key, input.head, new Date(window.getStart), new Date(window.getEnd)))
-          }
+    })
+
+    val keyed1Stream = filteredStream
+      .filter(event => event.actor1Geo_countryCode != null
+        && event.actor1Code_religion1Code != null)
+      .map(event => {
+        if (event.actor1Geo_countryCode.length != 2) {
+          System.err.println("Country code is not length 2: " + event.actor1Geo_countryCode)
         }
-    ).print
+        event
+      })
+      .map(event => GDELTEventWrapper(event, event.actor1Geo_countryCode, event.actor1Code_religion1Code.substring(0, 3), 1))
+      .keyBy(wrapper => wrapper.country + wrapper.religionPrefix)
+
+    //TODO keyed2Stream
+
+    val aggregated1Stream: DataStream[AccumulatorResult] = keyed1Stream.window(TumblingEventTimeWindows.of(Time.days(200))).aggregate(new ProjectNameAggregation())
+
+    aggregated1Stream.addSink(res => System.out.println("Result: " + res))
 
     env.execute("Flink Scala GDELT Analyzer")
 
   }
 
 }
+
+case class GDELTEventWrapper(gDELTEvent: GDELTEvent, country: String, religionPrefix: String, actorNumber: Int)
